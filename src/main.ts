24fcca3b598eeb05916fa9322c221fd8458d3d20
Barend Erasmus <hirebarend@gmail.com>
2025-01-18 11:01:40 +0100

    initial

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-console.log('Hello World');
+import fs from 'fs';
+import * as ort from 'onnxruntime-node';
+import { postprocess, preprocess } from './utils';
+
+(async () => {
+  const buffer: Buffer = fs.readFileSync('test.png'); // REPLACE
+
+  const { height, input, width } = await preprocess(buffer);
+
+  const model = await ort.InferenceSession.create('custom.onnx');
+
+  const tensor = new ort.Tensor(Float32Array.from(input), [1, 3, 640, 640]);
+
+  const outputs = await model.run({ images: tensor });
+
+  const result = postprocess(outputs['output0'].data, width, height);
+
+  console.log(result);
+})();
